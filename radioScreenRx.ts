924a48ac7eaaa390e5080933d2979414abcc1032
KierPalin <45743174+KierPalin@users.noreply.github.com>
2024-12-17 21:17:32 +0000

    documenting radioScreenRx, removing radioScreenTx.

---
+++ radioScreenRx.ts
@@ -0,0 +1,14 @@
+// init.ts needs to redirect to here.
+
+/**
+* Decode incoming data,
+* Map it to appropriate screen.ts function + params
+* Invoke it.
+*/
+export function radioScreenRx(): void {
+    decodeCommand();
+}
+
+function decodeCommand(): void {
+
+}
